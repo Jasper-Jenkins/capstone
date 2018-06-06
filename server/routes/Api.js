2d5aa71b776abda3740b5a355a21934bc631be2b
Ramsey Bland <ramsey.bland@gmail.com>
2018-06-05 21:43:19 -0600

    fixed api request

--- server/routes/Api.js
+++ server/routes/Api.js
@@ -3,7 +3,7 @@ var router = require('express').Router()
 let axios = require('axios')
 //import axios from 'axios'
 
-var api = axios.create({
+var api = axios.default.create({
   baseURL: 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=',
   timeout: 3000,
   withCredentials: true
@@ -11,12 +11,10 @@ var api = axios.create({
 
 router.get('/api/locations/:query', (req, res) => {
     console.log(req.params.query)
-  api.get("" + req.params.query + '&key=AIzaSyCb7mDvm7G1Aa7Pu3LklOArn99tbDJZj00')
+  api.get('' + req.params.query + '&key=AIzaSyCb7mDvm7G1Aa7Pu3LklOArn99tbDJZj00')
     .then(place => {
-      res.status(200).send(place)
-    })
-    .catch(err => {
-      res.status(400).send(err)
+      place
+      res.send(place.data)
     })
 })
 
