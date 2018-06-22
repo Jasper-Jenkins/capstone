179715ad03f841a57ab82a02d76d992711c30015
Brian Seward <blacktiger55@hotmail.com>
2018-06-22 13:24:18 -0600

    fixing pix home css

--- www/src/components/Home.vue
+++ www/src/components/Home.vue
@@ -1,78 +1,3 @@
-<template>
-  <div class="home">
-    <!-- <h1>/test</h1> -->
-    <div class="container-fluid jumbotron bghome">
-
-      
-    
-  <div class="textcolor mgmenu">
-          <div class="row justify-content-center bgmenu">
-      
-            
-            <div class="col-3 justify-content-center">
-              <create-trip></create-trip>
-              </div>
-
-            <div class="col-3">
-                <h4>Select Existing Trip</h4>
-                <select v-model="trip">
-                    <option disabled value=''>Add Destination to Trip: </option>
-                    <option v-for="trip in trips" :key="trip._id" :value="trip">{{trip.title}}</option>
-                  </select>
-                  </div>
-               
-            <div class="col-3 mt-2 justify-content-center">
-                <form @submit.prevent="getResults">
-                  <input type="text" name="destination" id="destination" placeholder=" Where To?" v-model="destination.title">
-                  <button class="btn btn-primary btn-success btn-sm" type="submit">Search</button>
-                </form>
-                </div>
-             
-              
-              </div>
-    </div>
-
-
-
-
-
-      <!-- EVERYTHING BELOW IS THE SEARCH RESULT LAYOUT -->
-      <div class="row">
-        <div class="col-12 mt-2">
-          <h4>Looking for destination ideas? Here are some ideas from other travelers.</h4>
-          <h4>Add new destinations below! </h4>
-        </div>
-      </div>
-
-      <!-- USER RESULTS -->
-        <div class="col userResults text-center" v-for="userResult in userResults" :key="userResult._id">=
-          <img :src="userResult.photo" alt="">
-          <a @click="selectActiveDest(userResult)">
-            <strong>{{userResult.title}}</strong>
-          </a>
-        </div>
-
-        
-      
-      <!-- THE DIV BELOW IS END OF CARD -->
-      </div>
-
-
-      <!-- GOOGLE RESULTS -->
-      <div class="row">
-        <div class="col-12 mt-2">
-        </div>
-        <div class="col googleResults text-center" v-for="result in apiResults" :key="result._id">
-          <img :src="result.photo" alt="">
-          <strong>{{result.name}}</strong>
-          <button class="btn btn-primary btn-success btn-sm" @click="addDestination(result)" v-if="trips.length != 0">Add To Trip</button>
-          <mytrip v-if="trips.length == 0"></mytrip>
-        </div>
-      </div>
-
-    </div>
-  </div>
-</template>
 
 
 
@@ -142,56 +67,170 @@
   };
 </script>
 
+
+
+<template>
+  <div class="home">
+    <!-- <h1>/test</h1> -->
+    
+    <section class="search vertical-center jumbotron bghome">
+
+      <div class="container-fluid">
+
+        <div class="textcolor mgmenu">
+
+          
+          <div class="row justify-content-center mx-auto bgmenu">  
+            <!-- create trip shit -->
+            <div class="col-sm-2 justify-content-center">
+              <create-trip></create-trip>
+              </div>
+
+            <div class="col-sm-2 mt-2 mb-2">
+                <h5>Select Existing Trip</h5>
+                <select v-model="trip">
+                    <option disabled value=''>Add Destination to Trip: </option>
+                    <option v-for="trip in trips" :key="trip._id" :value="trip">{{trip.title}}</option>
+                  </select>
+                  </div>
+               
+            <div class="col-sm-2 mt-2 ml-4">
+                <form @submit.prevent="getResults">
+                  <input type="text" name="destination" id="destination" placeholder=" Where To?" v-model="destination.title">
+                  <button class="btn btn-primary btn-success btn-sm mt-2 mb-2" type="submit">Search</button>
+                </form>
+                </div>
+                <!-- <div class="row">
+                    <div class="col-12 mt-2">
+                      <h4>Looking for destination ideas? Here are some ideas from other travelers.</h4>
+                      <h4>Add new destinations below! </h4>
+                    </div>
+                  </div> -->
+             
+              </div>
+        </div>
+
+      <!-- </section>
+      <section class="results jumbotron"> -->
+
+      <!-- EVERYTHING BELOW IS THE SEARCH RESULT LAYOUT -->
+      <!-- USER RESULTS -->
+      <div class="row ur mx-auto justify-content-center mt-2">   
+
+        <div class="col-2 userResults" v-for="userResult in userResults" :key="userResult._id">
+          <img :src="userResult.photo" alt="">
+          <a @click="selectActiveDest(userResult)">
+            <strong>{{userResult.title}}</strong>
+          </a>
+        </div>
+
+        </div>
+
+      <!-- GOOGLE RESULTS -->
+      <div class="row gr mx-auto mt-2">
+
+        <div class="col-2 googleResults" v-for="result in apiResults" :key="result._id">
+          <img :src="result.photo" alt="">
+          <strong>{{result.name}}</strong>
+          <button class="btn btn-primary btn-success btn-sm" @click="addDestination(result)" v-if="trips.length != 0">Add To Trip</button>
+          <mytrip v-if="trips.length == 0"></mytrip>
+        </div>
+
+      </div>
+
+
+    </div>
+</section>
+  </div>
+</template>
+
+
+
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+  .userResults {
+    flex: 25%;
+  max-width: 25%;
+  padding: 0 4px;
+
+    /* background: rgba(247, 247, 247, 0.589);
+      color: white;
+      border: 1px solid black;
+      margin: 5px;
+      padding: 10px; */
+      /* float: left; */
+  }
+  .googleResults {
+    flex: 25%;
+  max-width: 25%;
+  padding: 0 4px;
+
+
+
+    /* background: rgba(247, 247, 247, 0.589);
+      color: white;
+      border: 1px solid black;
+      margin: 5px;
+      padding: 10px; */
+      /* float: left; */
+  }
+    .userResults img {
+      margin-top: 8px;
+      vertical-align: middle;
+
+
+        /* height: 100px;
+        width: auto; */
+        /* float: right; */
+    }
+    
+    .googleResults img {
+      margin-top: 8px;
+      vertical-align: middle;
+
+
+        /* height: 100px;
+        width: auto; */
+        /* float: right; */
+    }
+    
+    .ur {
+      display: flex;
+      flex-wrap: wrap;
+      padding: 0 4px;
+    }
+    
+    .gr {
+      display: flex;
+      flex-wrap: wrap;
+      padding: 0 4px;
+    }  
+
+.bghome {
+  background: url(../assets/tresting.jpg);
+  /* background-repeat: no-repeat; */
+  margin-top: 2em;
+} 
 .textcolor{
   color: black;
 }
-
-.card {
-  background-color: rgba(248, 248, 248, 0.795);
-  color: black;
-}
-.bghome {
-  background: url(../assets/tresting.jpg);
-  background-repeat: no-repeat;
-} 
 .jumbotron {
   min-height: 600px; 
-}
-.userResults {
-    background-color: rgba(0, 0, 0, 0.9);
-    color: white;
-    border: 1px solid black;
-    margin: 5px;
-    padding: 10px;
-    /* float: left; */
-}
-.googleResults {
-    background-color: rgba(0, 0, 0, 0.9);
-    color: white;
-    border: 1px solid black;
-    margin: 5px;
-    padding: 10px;
-    /* float: left; */
+  /* justify-content: center; */
 }
 
-.userResults img {
-    height: 100px;
-    width: auto;
-    /* float: right; */
-}
-
-.googleResults img {
-    height: 100px;
-    width: auto;
-    /* float: right; */
-}
 
 .bgmenu {
-  background: rgba(248, 248, 248, 0.815);
-  margin-top: 10em;
-  margin-bottom: 45em;
+  background: rgba(247, 247, 247, 0.589);
+  border-color: rgba(0, 0, 0, 0.479);
+  border-width: 0.2mm;
+  border-style: solid;
+  /* justify-content: center; */
+}
+
+.results{
+  background: url(../assets/tresting.jpg);
 }
 
 /* h1,h2 {
