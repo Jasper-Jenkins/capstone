0e52913d143caae87946877490e99bd4b62c70cf
Ramsey Bland <ramsey.bland@gmail.com>
2018-07-14 23:45:43 -0600

    added masonry effect

--- www/src/components/Home.vue
+++ www/src/components/Home.vue
@@ -35,17 +35,17 @@
       }
     },
     mounted() {
-      debugger
       if (!this.$store.state.userModule.user._id) {
         router.push({ name: "User" });
       }
       this.$store.dispatch("getUsersTrips");
-      //this.$store.dispatch('clearResults')
+      this.masonry
     },
     methods: {
       getResults() {
         console.log(this.destination);
-        this.$store.dispatch("findDestination", this.destination.title); // incomplete only has a title at the moment
+        this.masonry()
+        this.$store.dispatch("findDestination", this.destination.title)
         this.title = "";
       },
       addDestination(result) {
@@ -58,6 +58,15 @@
       },
       selectDest(dest) {
         this.$store.dispatch("selectActiveDest", dest)
+      },
+      masonry() {
+        var elem = document.querySelector('.grid');
+        var msnry = new Masonry(elem, {
+          itemSelector: '.grid-item',
+          columnWidth: document.body.scrollWidth
+        });
+        var msnry = new Masonry('.grid', {
+        });
       }
     }
   };
@@ -68,84 +77,73 @@
 <template>
   <div class="home">
     <!-- <h1>/test</h1> -->
+    <div class="row justify-content-center mx-auto">
+      <!-- create trip-->
+      <div class="col-sm-2 justify-content-center">
+        <create-trip></create-trip>
+      </div>
 
-    <section class="search vertical-center jumbotron bghome">
-      <div class="container-fluid">
-        <div class="textcolor mgmenu">
-          <div class="row justify-content-center mx-auto bgmenu">
-            <!-- create trip-->
-            <div class="col-sm-2 justify-content-center">
-              <create-trip></create-trip>
-            </div>
-
-            <div class="col-sm-2 mt-2 mb-2">
-              <h5>Select Existing Trip</h5>
-            
-            </div>
-
-            <div class="col-sm-2 mt-2 ml-4">
-              <form @submit.prevent="getResults">
-                <input type="text" name="destination" id="destination" placeholder=" Where To?" v-model="destination.title">
-                <button class="btn btn-primary btn-success btn-sm mt-2 mb-2" type="submit">Search</button>
-              </form>
-            </div>
-          </div>
-        </div>
-        <!-- EVERYTHING BELOW IS THE SEARCH RESULT LAYOUT -->
-        <!-- USER RESULTS -->
-        <div class="rowing ur mt-2">
-
-          <div class="column userResults" v-for="userResult in userResults" :key="userResult._id">
-            <img :src="userResult.photo" alt="">
-            <div class="test">
-              <a @click="selectActiveDest(userResult)">
-                <strong>{{userResult.title}}</strong>
-              </a>
-            </div>
-            <p>Author: {{userResult.author}}</p>
-          </div>
-        </div>
-
-        <!-- GOOGLE RESULTS -->
-        <div class="rowing gr mt-2">
-          <div class="column googleResults d-flex flex-row flex-wrap">
-            <div v-for="result in apiResults" :key="result._id">
-              <img :src="result.photo" alt="">
-              <div class="test">
-                <strong>{{result.name}}</strong>
-                <button class="btn btn-primary btn-success btn-sm"  type="button" data-toggle="modal" data-target="#destModal" @click="selectDest(result)">Add To Trip</button>
-                <div class="modal fade" id="destModal" tabindex="-1" role="dialog" aria-labelledby="destModalTitle" aria-hidden="true">
-                  <div class="modal-dialog modal-dialog-centered" role="document">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h5 class="modal-title" id="destModalTitle">{{currentDest.name}}</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">&times;</span>
-                        </button>
-                      </div>
-                      <div class="modal-body">
-                        <h4>Select a Trip!</h4>
-                        <select v-model="trip">
-                          <option disabled value=''>Add Destination to Trip: </option>
-                          <option v-for="t in trips" :key="t._id" :value="t">{{t.title}}</option>
-                        </select>
-                      </div>
-                      <div class="modal-footer">
-                        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                        <button v-if="trip._id" class="btn btn-primary btn-primary" data-dismiss="modal" @click="addDestination(currentDest)">Submit</button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <mytrip v-if="trips.length == 0"></mytrip>
-            </div>
-          </div>
-        </div>
-
+      <div class="col-sm-2 mt-2 mb-2">
+        <h5>Select Existing Trip</h5>
 
       </div>
-    </section>
+
+      <div class="col-sm-2 mt-2 ml-4">
+        <form @submit.prevent="getResults">
+          <input type="text" name="destination" id="destination" placeholder=" Where To?" v-model="destination.title">
+          <button class="btn btn-primary btn-success btn-sm mt-2 mb-2" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+    <!-- EVERYTHING BELOW IS THE SEARCH RESULT LAYOUT -->
+    <!-- USER RESULTS -->
+    <div class="grid">
+      <div class="grid-item" v-for="userResult in userResults" :key="userResult._id">
+        <div class="card result">
+          <img :src="userResult.photo" alt="">
+          <div class="test">
+            <a @click="selectActiveDest(userResult)">
+              <strong>{{userResult.title}}</strong>
+            </a>
+          </div>
+          <p>Author: {{userResult.author}}</p>
+        </div>
+      </div>
+
+
+      <!-- GOOGLE RESULTS -->
+      <div class="grid-item" v-for="result in apiResults" :key="result._id">
+        <div class="card result">
+          <img :src="result.photo" alt="">
+          <strong>{{result.name}}</strong>
+          <button class="btn btn-primary btn-success btn-sm" type="button" data-toggle="modal" data-target="#destModal" @click="selectDest(result)">Add To Trip</button>
+          <div class="modal fade" id="destModal" tabindex="-1" role="dialog" aria-labelledby="destModalTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="destModalTitle">{{currentDest.name}}</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <h4>Select a Trip!</h4>
+                  <select v-model="trip">
+                    <option disabled value=''>Add Destination to Trip: </option>
+                    <option v-for="t in trips" :key="t._id" :value="t">{{t.title}}</option>
+                  </select>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                  <button v-if="trip._id" class="btn btn-primary btn-primary" data-dismiss="modal" @click="addDestination(currentDest)">Submit</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <mytrip v-if="trips.length == 0"></mytrip>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -153,67 +151,18 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-  .rowing {
-    display: flex;
-    flex-wrap: wrap;
-    padding: 0 4px;
+  .grid-item {
+    width: 33%;
   }
 
-  column {
-    flex: 25%;
-    max-width: 25%;
-    padding: 0 4px;
+  .result {
+    margin: 10px;
+    padding: 5px;
+    border: 2px solid black;
   }
 
-  @media screen and (max-width: 800px) {
-    .column {
-      flex: 50%;
-      max-width: 50%;
-    }
-  }
-
-  @media screen and (max-width: 600px) {
-    .column {
-      flex: 100%;
-      max-width: 100%;
-    }
-  }
-
-  .userResults img {
-    margin-top: 8px;
-    vertical-align: top;
-    float: top;
-  }
-
-  .googleResults img {
-    margin-top: 8px;
-    vertical-align: middle;
-    display: inline-block;
-  }
-
-  .bghome {
-    background: url(../assets/tresting.jpg);
-    margin-top: 2em;
-  }
-
-  .textcolor {
-    color: black;
-  }
-
-  .jumbotron {
-    min-height: 600px;
-  }
-
-
-  .bgmenu {
-    background: rgba(247, 247, 247, 0.589);
-    border-color: rgba(0, 0, 0, 0.479);
-    border-width: 0.2mm;
-    border-style: solid;
-  }
-
-  .results {
+  .home {
     background: url(../assets/tresting.jpg);
   }
+
 </style>
